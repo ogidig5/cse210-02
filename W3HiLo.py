683c7184cf234b7f4a5db287bff16faf77db130c
ogidig5 <georgeogidi18@gmail.com>
2022-01-22 22:47:43 +0300

    Added 2 lines of comments at the top

--- W3HiLo.py
+++ W3HiLo.py
@@ -1,9 +1,12 @@
 '''
-Authors: Dan Allred,
+Authors: Dan Allred, George Ogidi(Team 04),
 Edit 1/22/2022 10:08am EST. - DA - original commit
 '''
 
 import random
+# To create a python class, use the keyword 'class'
+# Use the __init__() function to assign values to object properties
+
 
 class Card(object):
     def __init__(self, suit, value):
@@ -19,7 +22,8 @@ class Card(object):
         return result
 
     def show(self):
-        print ("{} of {}".format(self.value, self.suit))
+        print("{} of {}".format(self.value, self.suit))
+
 
 class Deck(object):
     def __init__(self):
@@ -28,7 +32,8 @@ class Deck(object):
 
     def build(self):
         for suit in ["Spades", "Clubs", "Diamonds", "Hearts"]:
-            for value in ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]:  #Ace, Jack, queen and King need comparible. or we could just name them 1, 11, 12, 13
+            # Ace, Jack, queen and King need comparible. or we could just name them 1, 11, 12, 13
+            for value in ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]:
                 self.cards.append(Card(suit, value))
 
     def show(self):
@@ -36,24 +41,25 @@ class Deck(object):
             card.show()
 
     def shuffle(self):
-        for n in range(len(self.cards)-1,0,-1):
+        for n in range(len(self.cards)-1, 0, -1):
             r = random.randint(0, n)
             self.cards[n], self.cards[r] = self.cards[r], self.cards[n]
 
     def drawCard(self):
         return self.cards.pop()
 
+
 class Person(object):
     def __init__(self):
         self.hand = []
 
     def __gt__(self, comparing_obj):
-        
+
         result = self.hand[-1] > comparing_obj.nextcard[-1]
         return result
 
     def __lt__(self, comparing_obj):
-        
+
         result = self.hand[-1] < comparing_obj.nextcard[-1]
         return result
 
@@ -64,6 +70,7 @@ class Person(object):
     def show(self):
         for card in self.hand:
             card.show()
+
 
 class NextCard(object):
     def __init__(self):
@@ -78,7 +85,6 @@ class NextCard(object):
             card1.show()
 
 
-
 deck = Deck()
 deck.shuffle()
 
@@ -89,13 +95,12 @@ newcard = NextCard()
 nextCard = newcard.draw(deck)
 
 
-
 play = input("Do you want to play High/Low? Y or N? ")
-while play.lower()== "y":
+while play.lower() == "y":
     card = playerHand
     print("The current card is: ", str(card.show()))
-    guess = input("Guess H for high or L for low." )
-    if guess.lower()=="h":
+    guess = input("Guess H for high or L for low.")
+    if guess.lower() == "h":
         if card > nextCard:
             print(f"correct! The the card is {card.show()}")
             play = input("Play again? Y or N? ")
@@ -104,7 +109,7 @@ while play.lower()== "y":
             print(f"You lost! The card was {nextCard.show()}")
             play = input("Play again? Y or N? ")
 
-    if guess.lower()=="l":
+    if guess.lower() == "l":
         if card > nextCard:
             print(f"You lost! card was {nextCard.show()}")
             play = input("Play again? Y or N? ")
